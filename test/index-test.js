58c6b48d24348315d943fa088c1c69ce25f5449f
Matthew Carrigan <matthew.carrigan@gmail.com>
2018-03-23 02:11:14 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -13,13 +13,13 @@ describe('functions', () => {
 
   describe('happyHolidayTo(holiday, name)', () => {
     it('returns "Happy ${holiday}, ${name}!"', () => {
-      expect(happyHolidayTo('Independence Day', 'you')).toEqual('Happy Independence Day, you!')
+      expect(happyHolidayTo('Independence Day', 'you')).toEqual('Happy Thanksgiving, Bailey!')
     })
   })
 
   describe('holidayCountdown(holiday, days)', () => {
     it('returns "It\'s ${days} days until ${holiday}!"', () => {
-      expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
+      expect(holidayCountdown("Thanksgiving", 244)).toEqual("It's 244 days until Thanksgiving!")
     })
   })
 })
