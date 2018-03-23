07c361e7a02c31a0ddf37ac22d4c1a71ca9aa7ad
Matthew Carrigan <matthew.carrigan@gmail.com>
2018-03-23 01:52:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ function happyHolidaysTo(name) {
   return(`Happy holidays, ${name}!`)
 }
 
-function happyHolidaysTo(name) {
-  holiday = 'Halloween'
+function happyHolidaysTo(holiday, name) {
+  holiday = 'Thanksgiving'
   name = 'Bailey'
   return(`Happy ${holiday}, ${name}!`)
 }
 
+function holidayCountdown(days, holiday){
+  days = 
+  holiday = 'Thanksgiving'  
+  return(`It's ${days} days until ${holiday}!`)
+}
